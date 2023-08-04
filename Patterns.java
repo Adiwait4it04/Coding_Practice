6f4cee8b89910e9f292fd7484556676d3da7e2c4
Aditya Saha <adityasaha04@gmail.com>
2023-08-05 03:36:13 +0530

    ✨ New Pattern

---
+++ Patterns.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Patterns{
+    public static void main(String[] args){
+        // for(int i=0;i<=4;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+                   for(int i=0;i<=5;i++){
+               for(int j=0;j<=5;j++){
+                if(i==0||i==5){
+                    if(j==0||j==5){
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                else{
+                    System.out.print("*");
+                }
+               }
+               System.out.println();
+    }
+    }
+}
