eb0a44f63c8a4a6d2a821bef276db6d7e13172c7
Aditya Saha <adityasaha04@gmail.com>
2023-08-11 20:54:22 +0530

    ✨

--- Product_of_Array_Except_Self.java
+++ Product_of_Array_Except_Self.java
@@ -1,19 +1,24 @@
-class Solution {
+lass Solution {
     public int[] productExceptSelf(int[] nums) {
-      final int n = nums.length;
-      int[] ans = new int[n];
-      ans[0] = 1;
-  
-     
-      for (int i = 1; i < n; ++i)
-        ans[i] = ans[i - 1] * nums[i - 1];
-  
-      int suffix = 1; 
-      for (int i = n - 1; i >= 0; --i) {
-        ans[i] *= suffix;
-        suffix *= nums[i];
-      }
-  
-      return ans;
+        int[] left = new int[nums.length];
+        
+        int[] right = new int[nums.length];
+        
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i-1] * nums[i-1];
+        }
+        
+        right[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i+1] * nums[i+1];
+        }
+        
+        int[] product = new int[nums.length];
+        for (int i = 0; i < product.length; i++) {
+            product[i] = left[i] * right[i];
+        }
+        
+        return product;
     }
-  }
+}
