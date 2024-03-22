4eec2ed1700f167ce551fbbe7168926bf0692e32
Aditya Kumar Saha <adityasaha04@gmail.com>
2024-03-23 01:15:36 +0530

    ✨

--- StackFW.java
+++ StackFW.java
@@ -1,5 +1,17 @@
 import java.util.*;
-public class StackFW{
+public class StackFW
+{
+    public static void pushatbottom(int data,Stack<Integer> s){
+        if(s.isEmpty())
+        {
+            s.push(data);
+            return;
+        }        
+        int top = s.pop();
+        System.out.println(s);
+        pushatbottom(data,s);
+        s.push(top);
+    }
     public static void main(String args[]){
         Stack <Integer> n =new Stack<>();
         n.push(1);
@@ -7,6 +19,7 @@ public class StackFW{
         n.push(3);
         n.push(4);
         n.push(5);
+        pushatbottom(7,n);
         while(!n.isEmpty()){
             System.out.println(n.peek());
             n.pop();
