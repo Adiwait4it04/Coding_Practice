96360025fee75a574f7c875b158629e01f1d2c12
Adiwait4it04 <[adityasaha04@gmail.com]>
2024-07-18 17:05:54 +0530

    DSAS

--- climbingstairs.java
+++ climbingstairs.java
@@ -7,7 +7,7 @@ public class climbingstairs {
         arr1[1]=1;
         for(int i=2;i<arr.length;i++)
         {
-            arr[i]=arr[i-1]+arr[i-2];EE
+            arr[i]=arr[i-1]+arr[i-2];
         }
         System.out.println(rec(n,arr1));
     }
