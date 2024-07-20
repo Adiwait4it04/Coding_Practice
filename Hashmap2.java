c20b2d8f99e4006f5e069e6653a2b0157308c77d
Adiwait4it04 <[adityasaha04@gmail.com]>
2024-07-20 19:37:53 +0530

    🔵 other: added

--- Hashmap2.java
+++ Hashmap2.java
@@ -60,21 +60,40 @@ class Hashmap2
         //     start=map.get(start);
         //     map.remove(s);
         // }
+        // HashMap<Integer,Integer> map= new HashMap<>();
+        
+      
+        //largest subarray sums to 0
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     sum+=arr[i];
+        //     if (map.containsKey(sum)) 
+        //     {
+        //         len=Math.max(len, i-map.get(sum));
+        //     }
+        //     else
+        //     {
+        //         map.put(sum,i);
+        //     }
+        // }
+        // System.out.println(len);
+
+        //largest subarray sums to k
+        //sum(0,j) - sum(0,i)=sum(i+1,j)
         HashMap<Integer,Integer> map= new HashMap<>();
-        int arr[]={15,-2,2,-8,1,7,10};
         int sum=0;
         int len=0;
+        int arr[]={10,2,-2,-20,10};
+        int k=-10;
+        map.put(0, 1);
         for(int i=0;i<arr.length;i++)
         {
-            sum+=arr[i];
-            if (map.containsKey(sum)) 
+            sum += arr[i];
+            if (map.containsKey(sum-k)) 
             {
-                len=Math.max(len, i-map.get(sum));
+                len= len+map.get(sum-k);
             }
-            else
-            {
-                map.put(sum,i);
-            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);          
         }
         System.out.println(len);
     }
